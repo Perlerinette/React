965a8388b97d822cf3e754fa2d6d28d9deaf251c
Perlerinette <laurine.reyre@gmail.com>
2021-01-29 21:14:22 -0500

    Props complete

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -10,6 +10,7 @@ import FunctionalComponentDemo from '../concepts/FunctionalComponentDemo'
 import JSXRules from '../concepts/JSXRules'
 import State from '../concepts/State'
 import Effects from '../concepts/Effects'
+import PropsDemo from '../concepts/PropsDemo'
 
 const Sidebar = () => {
     return (
@@ -21,6 +22,7 @@ const Sidebar = () => {
                     <li><Link to="/jsxrules">JSX Rules</Link></li>
                     <li><Link to="/state">useState</Link></li>
                     <li><Link to="/effects">useEffect</Link></li>
+                    <li><Link to="/props">Props Demo</Link></li>
                     <li><Link to="/resources">Resources</Link></li>
                 </ul>
             </div>
@@ -32,6 +34,7 @@ const Sidebar = () => {
                     <Route exact path="/jsxrules"><JSXRules /></Route>
                     <Route exact path="/state"><State /></Route>
                     <Route exact path="/effects"><Effects /></Route>
+                    <Route exact path="/props"><PropsDemo /></Route>
                     <Route exact path="/functionalcomponent"><FunctionalComponentDemo /></Route>
                 </Switch>
             </div>
